602d4421e18dbaaf486c89cd4bc96c611641b8fd
Sarah Mehri <smehri@mail.greenriver.edu>
2021-01-22 22:05:06 -0500

    adding dupRemove

--- function.php
+++ function.php
@@ -1,15 +1,17 @@
 <?php
 
 echo"<p> PHP Array Practice</p>";
-$numbers = array(7, 9, 8, 9, 8, 8, 6);
 
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+//printing numbers
 function printArr($numbers){
 $round = count($numbers);
 for($i =0; $i<$round; $i++){
-echo "<p> $numbers[$i] <br> </p>";
+    echo "<p> $numbers[$i] <br> </p>";
 }
 
 }
+//calling number function
 printArr($numbers);
 
 function largest($numbers){
@@ -24,4 +26,14 @@ function largest($numbers){
     echo "<p> The largest number in array is : $j <br> </p>";
 
 }
+//calling largest
 largest($numbers);
+
+//printing the array without the dup
+function removeDups($numbers){
+
+    $results = array_unique($numbers);
+    echo "[" . implode(", ", $results) . "]";
+}
+//calling the removeDups function
+removeDups($numbers);
