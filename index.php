d2a06f0e7756603f8e1a6384cb9977bd6bf76978
Sarah Mehri <smehri@mail.greenriver.edu>
2021-01-22 21:50:23 -0500

    modifying

--- index.php
+++ index.php
@@ -12,6 +12,22 @@ include ("function.php");
 
 ?>
 
+<?php
+echo"<p> PHP Array Practice</p>";
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+
+function printArr($numbers){
+    $round = count($numbers);
+    for($i =0; $i<$round; $i++){
+        echo "<p> $numbers[$i] <br> </p>";
+    }
+
+}
+printArr($numbers);
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
