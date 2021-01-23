ed764c4327f6c865b8bc4a1112e176fc48c058f9
Sarah Mehri <smehri@mail.greenriver.edu>
2021-01-22 21:43:04 -0500

    adding index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/* Sarah Mehri
+ * 1/15/2020
+ * Pair Programing 2
+ */
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+include ("function.php");
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
