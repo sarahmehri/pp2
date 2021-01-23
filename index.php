79e178a059d7d8cd8f63fed6776a35a82b5bf640
Sarah Mehri <smehri@mail.greenriver.edu>
2021-01-22 21:53:54 -0500

    add function.php

--- index.php
+++ index.php
@@ -12,21 +12,7 @@ include ("function.php");
 
 ?>
 
-<?php
-echo"<p> PHP Array Practice</p>";
-$numbers = array(7, 9, 8, 9, 8, 8, 6);
-
-function printArr($numbers){
-    $round = count($numbers);
-    for($i =0; $i<$round; $i++){
-        echo "<p> $numbers[$i] <br> </p>";
-    }
-
-}
-printArr($numbers);
-
-
-?>
+<script src="function.php"></script>
 
 <!doctype html>
 <html lang="en">
